378e5c4b2a5ec7969d5c90a4f3b1fec4f591c5c6
Folu Aderibgbe <127684059+6lvcknight@users.noreply.github.com>
2024-05-10 16:01:26 -0400

    complete account creation function
    completed the backend and front end for creating accounts

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 
 auth = Blueprint('auth', __name__)
@@ -9,11 +10,27 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password1')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return '<p>its incomplete</p>'
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -24,20 +41,45 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
-            flash('Email is too short.', category='error')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
         elif len(firstName) < 2:
             flash("First name is too short.", category='error')
         elif len(lastName) <  2:
             flash("Last name is too short.", category='error')
         elif password1 != password2:
             flash("Passwords do not match.", category='error')
+        elif len(email) < 4:
+            flash('Email is too short.', category='error')   
         else:
             new_user = User(email=email, first_name=firstName, last_name=lastName, 
-                            password=generate_password_hash(password1, method='sha256'))
+                            password=generate_password_hash(password1, method='pbkdf2:sha256'))
             db.session.add(new_user)
             db.session.commit()
             flash("Account created.", category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("signup.html")
+    return render_template("signup.html", user=current_user)
+
+@auth.route('/forgotpassword', methods=['GET', 'POST'])
+def fpassword():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            flash("Email does not exist.", category='error')
+            return render_template("signup.html", user=current_user)
+        if password1 != password2:
+            flash("Passwords do not match.", category='error')
+        else:
+            user.password = generate_password_hash(password1, method='pbkdf2:sha256')
+            db.session.commit()
+            flash("Password has been updated.", category='success')
+            return render_template("login.html", user=current_user)
+
+
+    return render_template("forgot_password.html", user=current_user)
